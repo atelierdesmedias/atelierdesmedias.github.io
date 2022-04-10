3ed44d30a5119e634748175fe4cc1ed155ff1d8a
Martin Delille <martin@delille.org>
2022-04-10 22:58:04 +0200

    Use Koala gem for facebook operation

--- Gemfile
+++ Gemfile
@@ -3,7 +3,10 @@
 source 'https://rubygems.org'
 
 gem 'jekyll', '~> 4.2'
+gem 'koala', '~> 3.1.0'
+gem 'octokit', '~> 4.0'
 gem 'rake', '~> 13.0.1'
+gem 'rbnacl', '~> 7.1.1'
 gem 'rubocop', '~> 1.22.3'
 gem 'stringex', '~> 2.8.5'
 
--- events.rb
+++ events.rb
@@ -1,61 +1,36 @@
 # frozen_string_literal: true
 
-require 'time'
-require 'json'
-require 'net/http'
+require 'koala'
 require 'open-uri'
 require 'stringex'
-require 'uri'
+require 'time'
 
 require './env_utils'
 
-url = get_env_or_exit('FACEBOOK_GRAPH_URL')
+access_token = get_env_or_exit('FACEBOOK_TOKEN')
 page_id = get_env_or_exit('FACEBOOK_PAGE_ID')
-token = get_env_or_exit('FACEBOOK_TOKEN')
 
-uri = URI(url + page_id)
-uri.query = URI.encode_www_form({
-                                  fields: 'events',
-                                  access_token: token
-                                })
-res = Net::HTTP.get_response(uri)
-if res.is_a?(Net::HTTPSuccess)
-  Dir['_events/*.*'].each do |f|
-    puts "Deleting #{f}..."
-    File.delete(f)
+graph = Koala::Facebook::API.new(access_token)
+events = graph.get_connections(page_id, 'events', fields: %w[id name start_time place description cover])
+events.each do |event|
+  puts '---'
+  pp event
+  slug = event['name'].to_url
+  File.open("_events/#{slug}.md", 'w') do |f|
+    f.write("---\n")
+    f.write("name: \"#{event['name']}\"\n")
+    f.write("event_time: \"#{Time.parse(event['start_time'])}\"\n")
+    f.write("place: \"#{event['place']['name']}\"\n")
+    f.write("event_id: #{event['id']}\n")
+    f.write("---\n\n")
+    description = event['description']
+    f.write("\n#{description}\n") unless description.nil? || description.empty?
   end
-  events = JSON.parse(res.body)['events']['data']
-  events.each do |event|
-    puts '---', event
-    slug = event['name'].to_url
-    File.open("_events/#{slug}.md", 'w') do |f|
-      f.write("---\n")
-      f.write("name: \"#{event['name']}\"\n")
-      f.write("event_time: \"#{Time.parse(event['start_time'])}\"\n")
-      f.write("place: \"#{event['place']['name']}\"\n")
-      f.write("event_id: #{event['id']}\n")
-      f.write("---\n\n")
-      description = event['description']
-      f.write("\n#{description}\n") unless description.nil? || description.empty?
-    end
 
-    # Fetch event cover
-    uri = URI(url + event['id'])
-    uri.query = URI.encode_www_form({
-                                      fields: 'cover',
-                                      access_token: token
-                                    })
-    res = Net::HTTP.get_response(uri)
-    next unless res.is_a?(Net::HTTPSuccess)
-
-    cover_source = JSON.parse(res.body)['cover']['source']
-    URI.parse(cover_source).open do |image|
-      File.open("_events/#{slug}.jpg", 'wb') do |file|
-        file.write(image.read)
-      end
+  # Fetch event cover
+  URI.parse(event['cover']['source']).open do |image|
+    File.open("_events/#{slug}.jpg", 'wb') do |file|
+      file.write(image.read)
     end
   end
-else
-  puts "Bad response: #{res}"
-  exit(-1)
 end
