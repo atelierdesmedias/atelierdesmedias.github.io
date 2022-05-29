9e6da2f94d2214a2096f13f5450f895e263a3a61
Martin Delille <martin@delille.org>
2022-05-29 17:33:22 +0200

    Fetch only facebook event with name and cover.

--- events.rb
+++ events.rb
@@ -13,15 +13,19 @@ page_id = get_env_or_exit('FACEBOOK_PAGE_ID')
 graph = Koala::Facebook::API.new(access_token)
 events = graph.get_connections(page_id, 'events', fields: %w[id name start_time place description cover])
 events.each do |event|
+  next unless event['name'] && event['cover']
+
   puts '---'
   pp event
-  slug = event['name'].to_url
+  date = Time.parse(event['start_time'])
+  slug = "#{date.strftime('%F')}-#{event['name'].to_url}"
   File.open("_events/#{slug}.md", 'w') do |f|
     f.write("---\n")
     f.write("name: \"#{event['name']}\"\n")
-    f.write("event_time: \"#{Time.parse(event['start_time'])}\"\n")
-    f.write("place: \"#{event['place']['name']}\"\n")
+    f.write("event_time: \"#{date}\"\n")
+    f.write("place: \"#{event['place']['name']}\"\n") if event['place']
     f.write("event_id: #{event['id']}\n")
+    f.write("cover: #{slug}.jpg\n")
     f.write("---\n\n")
     description = event['description']
     f.write("\n#{description}\n") unless description.nil? || description.empty?
