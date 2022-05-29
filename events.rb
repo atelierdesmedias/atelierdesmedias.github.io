2f82b8800c8439791516e7e59252e91eb4c48316
Martin Delille <martin@delille.org>
2022-05-29 19:39:36 +0200

    Event script use yaml gem

--- events.rb
+++ events.rb
@@ -4,6 +4,7 @@ require 'koala'
 require 'mini_magick'
 require 'stringex'
 require 'time'
+require 'yaml'
 
 require './env_utils'
 
@@ -11,28 +12,27 @@ access_token = get_env_or_exit('FACEBOOK_TOKEN')
 page_id = get_env_or_exit('FACEBOOK_PAGE_ID')
 
 graph = Koala::Facebook::API.new(access_token)
-events = graph.get_connections(page_id, 'events', fields: %w[id name start_time place description cover])
-events.each do |event|
-  next unless event['name'] && event['cover']
+fb_events = graph.get_connections(page_id, 'events', fields: %w[id name start_time place description cover])
+fb_events.each do |fb_event|
+  next unless fb_event['name'] && fb_event['cover']
 
   puts '---'
-  pp event
-  date = Time.parse(event['start_time'])
-  slug = "#{date.strftime('%F')}-#{event['name'].to_url}"
+  pp fb_event
+  event_time = Time.parse(fb_event['start_time'])
+  slug = "#{event_time.strftime('%F')}-#{fb_event['name'].to_url}"
+  event = { 'name' => fb_event['name'],
+            'event_time' => event_time,
+            'event_id' => fb_event['id'],
+            'cover' => "#{slug}.jpg" }
+  event['place'] = (fb_event['place']['name']).to_s if fb_event['place']
   File.open("_events/#{slug}.md", 'w') do |f|
-    f.write("---\n")
-    f.write("name: \"#{event['name']}\"\n")
-    f.write("event_time: \"#{date}\"\n")
-    f.write("place: \"#{event['place']['name']}\"\n") if event['place']
-    f.write("event_id: #{event['id']}\n")
-    f.write("cover: #{slug}.jpg\n")
-    f.write("---\n\n")
-    description = event['description']
+    f.write("#{event.to_yaml}---\n")
+    description = fb_event['description'].gsub(/\r/, '')
     f.write("\n#{description}\n") unless description.nil? || description.empty?
   end
 
   # Fetch event cover
-  image = MiniMagick::Image.open(event['cover']['source'])
+  image = MiniMagick::Image.open(fb_event['cover']['source'])
   image.resize("x300\>")
   image.write("_events/#{slug}.jpg")
 end
