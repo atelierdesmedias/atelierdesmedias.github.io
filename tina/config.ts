48a09fc92398d1bc6924132d56e7c5ab274523e5
Martin Delille <martin@delille.org>
2023-05-01 11:34:01 +0200

    Add post collection

--- tina/config.ts
+++ tina/config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "tinacms";
 import { postFields } from "./templates";
+import moment from 'moment';
 
 const BRANCH = process.env.HEAD || process.env.BRANCH_NAME || "main";
 const CLIENT_ID = String(process.env.TINA_CLIENT_ID ?? '');
@@ -24,12 +25,71 @@ export default defineConfig({
     collections: [
       {
         format: "md",
+        label: "Articles",
+        name: "posts",
+        path: "_posts",
+        ui: {
+          defaultItem: {
+            date: moment(new Date()).format('YYYY-MM-DD'),
+            author: "La rédaction",
+            layout: "post",
+          },
+          filename: {
+            readOnly: true,
+            slugify: values => {
+              return `${values?.date?.substring(0, 10) ||
+                'xxxx'}-${values?.title?.toLowerCase().replace(/[^\w\s]/gi, '').replace(/ /g, '-').normalize('NFD').replace(/[\u0300-\u036f]/g, "")}`
+            },
+          },
+        },
+        fields: [
+          {
+            label: 'Titre',
+            name: 'title',
+            type: 'string',
+            isTitle: true,
+            required: true,
+          },
+          {
+            label: "Date",
+            name: "date",
+            type: "datetime",
+            required: true,
+          },
+          {
+            label: "Image d'en-tête",
+            name: 'image',
+            type: 'image',
+            required: true,
+          },
+          {
+            label: 'Auteur',
+            name: 'author',
+            type: 'string',
+            required: true,
+          },
+          {
+            label: "Corps du document",
+            name: "body",
+            type: "rich-text",
+            description: "This is the markdown body",
+            isBody: true,
+            required: true,
+          },
+          {
+            label: 'Layout',
+            name: 'layout',
+            type: 'string',
+            required: true,
+            ui: { component: 'hidden' },
+          },
+        ],
+      },
+      {
+        format: "md",
         label: "Formules",
         name: "formules",
         path: "_formulas",
-        match: {
-          include: "**/*",
-        },
         fields: [
           {
             type: "rich-text",
